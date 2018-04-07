aa1d1367ecaeff57ff5d27693f173f2aac35a2d6
David Ellam <dave_ellam@hotmail.com>
2018-04-07 17:55:31 +0100

    minor refactoring for clarity

--- src/main/scala/BillCalculator.scala
+++ src/main/scala/BillCalculator.scala
@@ -5,17 +5,30 @@ case class BillCalculator(menu: HashMap[String, BigDecimal]) {
 
   def calculateBillTotal(order: List[String]): BigDecimal = {
     val totalBeforeService = orderTotal(order)
-    val servicePercent = serviceApplicableFor(order)
-    val serviceAmount = (totalBeforeService * servicePercent).min(20.00)
-    val totalIncludingService = totalBeforeService + serviceAmount
-    totalIncludingService.setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    val serviceAmount = calculateServiceAmount(totalBeforeService, order)
+    roundedTotalAmount(totalBeforeService, serviceAmount)
   }
 
   private def orderTotal(order: List[String]): BigDecimal = {
     (order map menu).sum
   }
 
+  private def calculateServiceAmount(totalBeforeService: BigDecimal, order: List[String]): BigDecimal = {
+    val servicePercent = serviceApplicableFor(order)
+    val serviceAmount = (totalBeforeService * servicePercent).min(20.00)
+    serviceAmount
+  }
+
   private def serviceApplicableFor(order: List[String]): BigDecimal = {
-    if (order.contains("Cheese Sandwich")) 0.1 else if (order.contains("Steak Sandwich")) 0.2 else 0.0
+    order match {
+      case _ if order.contains("Cheese Sandwich") => 0.1
+      case _ if order.contains("Steak Sandwich")  => 0.2
+      case _ => 0.0
+    }
+  }
+
+  private def roundedTotalAmount(totalBeforeService: BigDecimal, serviceAmount: BigDecimal): BigDecimal = {
+    val totalIncludingService = totalBeforeService + serviceAmount
+    totalIncludingService.setScale(2, BigDecimal.RoundingMode.HALF_UP)
   }
 }
